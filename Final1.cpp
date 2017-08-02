69f014ab690f9f112a498a61516ae1cc9b04cbb6
Jennifer D <jenzlenz@gmail.com>
2017-08-01 23:54:25 -0400

    Create Final1.cpp
    created a default constructor for poly class - compiles without error

---
+++ Final1.cpp
@@ -0,0 +1,32 @@
+/* 
+Program Name: Final1.cpp
+Author: J Dooley
+Date: August 1, 2017
+Synopsis:
+Design and code a polynomial class in C++
+*/
+
+#include <iostream>
+using namespace std;
+
+class Poly
+{
+private:
+  int order;
+  int *coefficient;
+
+public:
+  Poly();  //default constructor
+};
+
+int main()
+{
+
+Poly P3; //create a poly p3 using default constructor
+
+return 0;
+}
+
+Poly::Poly(){
+
+}
