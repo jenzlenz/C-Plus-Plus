c258b392377cd42b49b2f391363432ac80ab71aa
Jennifer D <jenzlenz@gmail.com>
2017-08-03 01:18:18 -0400

    Update Final1.cpp
    default, one param and two param constructors, deconstructor all defined and working

--- Final1.cpp
+++ Final1.cpp
@@ -7,6 +7,7 @@ Design and code a polynomial class in C++
 */
 
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 class Poly
@@ -16,17 +17,45 @@ private:
   int *coefficient;
 
 public:
-  Poly();  //default constructor
+  Poly();
+  Poly(int order);
+  Poly(int order, int *coefficient);   
+  ~Poly();
 };
 
 int main()
 {
+  int myarray1[] = {2, 3, -12, 1, -19};
+  int myarray2[] = {2, 0, 7, 0, 0, -6, 1 -19};
 
-Poly P3; //create a poly p3 using default constructor
+  Poly P3; //create a poly p3 using default constructor
+  Poly P1(1); //create poly with order zero,  all coeffs 1
+  Poly P2(1,myarray1); //doesn't compile
 
 return 0;
 }
 
-Poly::Poly(){
+Poly::Poly():order(0),coefficient(NULL){
+  cout << "default constructor" << endl;
+}
 
+Poly::Poly(int Order):order(Order){
+  coefficient = new int[Order+1];
+  for(int i = 0; i < (Order + 1); ++i)
+     coefficient[i] = 1;
+  order = Order;
+  cout << "Parameterized constructor - only one parameter" << endl;
+}
+
+Poly::Poly(int Order, int *Coefficient):order(Order){
+  coefficient = new int[Order+1];
+  for(int i = 0; i < (Order + 1); ++i)
+     coefficient[i] = Coefficient[i];
+  order = Order;
+  cout << "Parameterized constructor - 2 parameters" << endl;
+}
+ 
+Poly::~Poly(){
+  delete []coefficient; //deallocate memory on the heap
+  cout << "deconstructor" << endl;
 }
