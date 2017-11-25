fe29ce274a2672317f2099c31f9458748b6d8d4f
Jennifer D <jenzlenz@gmail.com>
2017-11-25 01:36:54 -0500

    Added Triangle, Rectangle classes

--- AdvMidT1.cpp
+++ AdvMidT1.cpp
@@ -14,7 +14,7 @@ enum COLOR {red, orange, yellow, green, blue, indigo, violet};
 
 class Shape
 {
-private:
+protected:
 //member variables
 int x;
 int y;
@@ -46,21 +46,113 @@ COLOR getColor() const;
 
 int Shape::count = 0; // initialization of static int count variable must be outside of class
 
+class Triangle:public Shape
+{
+private:
+int base;
+int height;
+
+public:
+//constructors
+Triangle(); //default constructor
+Triangle(const Triangle &rhs); //copy constructor
+~Triangle(); //destructor
+
+//member functions
+float calcArea();
+void drawObject();
+};
+
+class Rectangle:public Shape
+{
+private:
+int base;
+int height;
+
+public:
+//constructors
+Rectangle(); //default constructor
+Rectangle(const Rectangle &rhs); //copy constructor
+~Rectangle(); //destructor
+
+//member functions
+float calcArea();
+void drawObject();
+};
+
+class Circle:public Shape
+{
+private:
+float radius;
+
+public:
+//constructors
+Circle(); //default constructor
+Circle(const Circle &rhs); //copy constructor
+~Circle(); //destructor
+
+//member functions
+float getRadius();
+float calcArea();
+void drawObject();
+};
+
+class Doughnut:public Circle
+{
+private:
+float radius;
+
+public:
+//constructors
+Doughnut(); //default constructor
+Doughnut(const Doughnut &rhs); //copy constructor
+~Doughnut(); //destructor
+
+//member functions
+float calcArea();
+void drawObject();
+};
+
+
+
+
 int main()
 {
-Shape A;
-A.setX(5);
-A.setY(10);
-cout << " The X of object A is equal to : " << A.getX() << endl;
-cout << " The Y of object A is equal to : " << A.getY() << endl;
-Shape B = A;
-cout << "The area of object A is: " << A.calcArea() << endl;
-A.drawObject();
-A.resetLocation(8,8);
-cout << " The X of object A after resetLocation is equal to : " << A.getX() << endl;
-cout << " The Y of object A after resetLocation is equal to : " << A.getY() << endl;
-cout << "The area of object A is: " << A.getArea() << endl;
-cout << "The color of object A is: " << A.getColor() << endl;
+
+//Testing Shape Base Class
+//Shape A;
+//A.setX(5);
+//A.setY(10);
+//cout << " The X of object A is equal to : " << A.getX() << endl;
+//cout << " The Y of object A is equal to : " << A.getY() << endl;
+//Shape B = A;
+//cout << "The area of object A is: " << A.calcArea() << endl;
+//A.drawObject();
+//A.resetLocation(8,8);
+//cout << " The X of object A after resetLocation is equal to : " << A.getX() << endl;
+//cout << " The Y of object A after resetLocation is equal to : " << A.getY() << endl;
+//cout << "The area of object A is: " << A.getArea() << endl;
+//cout << "The color of object A is: " << A.getColor() << endl;
+
+//Testing Circle Class
+Circle C;
+cout << "The radius of Circle C is: " << C.getRadius() << endl;
+cout << "The area of Circle C is: " << C.calcArea() << endl;
+C.drawObject();
+Circle D = C;
+
+//Testing Triangle Class
+Triangle T;
+cout << "The area of Triangle T is: " << T.calcArea() << endl;
+T.drawObject();
+Triangle U = T;
+
+//Testing Rectangle Class
+Rectangle R;
+cout << "The area of Rectangke R is: " << R.calcArea() << endl;
+R.drawObject();
+Rectangle S = R;
+
 return 0;
 }
 
@@ -136,4 +228,79 @@ COLOR Shape::getColor()const{
 //cout << "Inside SHAPE get color function - color = : " << color << endl;
 return color;
 }
+
+Circle::Circle(){
+cout << "CIRCLE default constructor: count = " << count << endl;
+}; //default constructor
+
+Circle::Circle(const Circle &rhs){
+//x = rhs.X;
+//y = rhs.Y;
+radius = rhs.radius;
+}; //copy constructor
+
+Circle::~Circle(){
+cout << "CIRCLE destructor: count = " << count << endl;
+}; //destructor
+
+//member functions
+float Circle::getRadius(){
+return (radius = 5.5);
+};
+
+float Circle::calcArea(){
+//cout << "Calculating Area for CIRCLE" << endl;
+return 99.99;
+};
+
+void Circle::drawObject(){
+cout << "Drawing CIRCLE" << endl;
+}
+
+Triangle::Triangle(){
+cout << "TRIANGLE default constructor: count = " << count << endl;
+}; //default constructor
+
+Triangle::Triangle(const Triangle &rhs){
+//x = rhs.X;
+//y = rhs.Y;
+//radius = rhs.radius;
+}; //copy constructor
+
+Triangle::~Triangle(){
+cout << "TRIANGLE destructor: count = " << count << endl;
+}; //destructor
+
+float Triangle::calcArea(){
+//cout << "Calculating Area for TRIANGLE" << endl;
+return 33.33;
+};
+
+void Triangle::drawObject(){
+cout << "Drawing TRIANGLE" << endl;
+}
+
+Rectangle::Rectangle(){
+cout << "RECTANGLE default constructor: count = " << count << endl;
+}; //default constructor
+
+Rectangle::Rectangle(const Rectangle &rhs){
+//x = rhs.X;
+//y = rhs.Y;
+//radius = rhs.radius;
+}; //copy constructor
+
+Rectangle::~Rectangle(){
+cout << "RECTANGLE destructor: count = " << count << endl;
+}; //destructor
+
+float Rectangle::calcArea(){
+//cout << "Calculating Area for RECTANGLE" << endl;
+return 44.44;
+};
+
+void Rectangle::drawObject(){
+cout << "Drawing RECTANGLE" << endl;
+}
+
 
