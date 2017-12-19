74f639cb9ab53494d803107cc0c85c785c9a6818
Jennifer D <jenzlenz@gmail.com>
2017-12-18 23:36:58 -0500

    Hash table working for declaration of independence

--- AdvFinal4.cpp
+++ AdvFinal4.cpp
@@ -1,67 +1,123 @@
-#include <iostream?
+#include <iostream>
 #include <cstring>
 #include <string.h>
 using namespace std;
 
-#define HASHLENGTH 13
+#define HLENGTH 13		//length fo Hash Vector
 
-int WordHash(string Word) {
-  int len = strlen(Word.c_str());
-  int sum = 0;
-  unsigned int key = -1;
-  
-  for(int i = 0; i < len; ++i)
-    sum = sum + Word[i];
-  key = sum % HASHLENGTH;
-  return key;
+int WordHash(string Word){
+	int len = strlen(Word.c_str());
+	int Sum = 0;
+	unsigned int key = -1;
+	for(int i = 0; i < len; ++i)
+		Sum = Sum + Word[i];
+	key = Sum % HLENGTH;
+	return key;
 }
 
-struct ListNode {
-  string Item;
-  string Desc;
-  int count; //the which of to and
-  ListNode *link;
+struct ListNode{
+	string Item;	//item name
+	ListNode *link; //LinkList pointer
 };
 
-ListNode* create(ListNode & node){
-  ListNode *current = new ListNode;
-  current->link = NULL;
-  current->item = node.Item;
-  current->Desc = node.Desc;
-  current->count = node.count;
-  
-  return current;
+
+ListNode*  create(ListNode & node){
+	ListNode *current = new ListNode;	//create 1st node
+	current->link = NULL;			    //set link pointer to NULL
+	current->Item = node.Item;
+	return current;
 }
 
-ListNode* add(ListNode* head, ListNode &node){
-  ListNode *current = new ListNode;
-  current->Item = node.item;
-  current->Desc = node.Desc;
-  current->count = node.count;
-  current->link = head;
-  head = current;
-  return head;
+ListNode*  add(ListNode * head, ListNode &node){
+	ListNode *current = new ListNode;   //create new node
+	current->Item = node.Item;
+	current->link = head;			    //add as a first node
+	head = current;			            // update head address to be current
+	return head;						// return new head address
+} 
+		
+void PrintList(ListNode * head){
+    ListNode * temp = head;				//copy head address to temp
+    cout << "  " << endl;
+	if(temp == NULL) 
+	   cout << "Empty List " << endl;
+	while(temp != NULL) {
+		cout << "word = " << temp->Item << endl;
+		temp = temp->link;				//move to next node
+	}
 }
 
-void PrintList(ListNode* head) {
-  ListNode* temp = head;
-  cout << " " << endl;
-  if(temp == NULL)
-    cout <<"Empty List" << endl;
-    
-  while(temp != NULL) {
-    cout << "temp->Item = " temp->Item << endl;
-    cout << "temp->Desc = " temp->Desc << endl;
-    cout << "temp->count = " temp->count << endl;
-    temp = temp->link;
-  }
-return;
-}
+int find(ListNode * head, string Word){
+     ListNode * temp = head;
+     int NodeId = 0;		//init NodeId
+     int wordCount = 0;
+	 while(temp != NULL) {
+    	if(temp->Item == Word){
+    		wordCount++;
+		}
+		NodeId++;
+		temp = temp->link;			//move to next node
+	}
+	cout << Word << " occurs " << wordCount << " times." << endl << endl << endl;
+	return NodeId;
+
+} 
 
 int main()
-{
+{	
+	string Word;
+	ListNode node;
+    struct ListNode* HashTable[HLENGTH] = {NULL};		
+    int key;
+    char text[] = "when in the course of human events it becomes necessary for one people "  
+    "to dissolve the political bands which have connected them with another and to assume " 
+    "among the powers of the earth the separate and equal station to which the Laws of "
+    "Nature and of Natures God entitle them a decent respect to the opinions of mankind "
+    "requires that they should declare the causes which impel them to the separation. "
+    "We hold these truths to be self-evident that all men are created equal that they "
+    "are endowed by their Creator with certain unalienable Rights that among these are "
+    "Life Liberty and the pursuit of Happiness. That to secure these rights Governments "
+    "are instituted among Men, deriving their just powers from the consent of the "
+    "governed # ";
+    char *token = std::strtok(text, " "); //get words from the char array
+    while (token != NULL) {
+        std::cout << token << '\n';
+        token = std::strtok(NULL, " ");
+	    node.Item = token;
+	    if(node.Item[0] == '#')break;
+	
+		key =  WordHash(node.Item);
+		if(HashTable[key] == NULL){
+			HashTable[key] = create(node);
+		} else {
+		    HashTable[key] = add(HashTable[key], node);
+		}
+	}
+	    
 
-char text[] = "When in the course of human events it becomes necessary for one people to dissolve th epolitical bands";
+	//print List
+	for(int i = 0; i < HLENGTH; ++i){
+	cout << " ---------- " << i << "-------------" << endl;
+	PrintList(HashTable[i]);
+	}
+	cout << endl;
 
-return 0;
+	//find a Word
+        while(1){
+		cout << endl << "Enter Word to Find:  #(quit)" << endl;
+		cin >> Word;
+		if(Word[0] == '#')break;
+		key = WordHash(Word);
+        cout << Word << " should be found in HashTable # " << key << endl;
+		int NodeId = find(HashTable[key], Word);
+        //cout << "NodeId = " << NodeId << endl;
+        if(NodeId != 0){
+			cout << "Print all words in HashTable " << endl;
+			PrintList(HashTable[key]);
+		}
+		else
+			cout << "Word not Found " << endl;
+	}
+
+	return 0;
 }
